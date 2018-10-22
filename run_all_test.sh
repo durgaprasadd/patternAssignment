bff1079b77e6ce7ecc10443a9c146cb67d53def2
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-10-22 13:16:30 +0530

    Removed old scripts that have been migrated.

--- run_all_test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-./run_test.sh ./create_rectangle.js ./test/input_file_rectangle ./test/output_file_rectangle
-./run_test.sh ./create_diamond.js ./test/input_file_diamond ./test/output_file_diamond
-./run_test.sh ./create_triangle.js ./test/input_file_triangle ./test/output_file_triangle
